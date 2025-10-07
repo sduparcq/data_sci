b2552da0d0d0acc085ee67683568210ccd36348e
sduparcq <stephane.duparcq77950@gmail.com>
2025-10-07 10:26:12 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # data_sci
+
+### Activer le .venv
+windows : .venv/Scripts/Activate.ps1
+
+### Features
+
+- [ ] nombre de truc executer par l'utilisateur --> faire un split de la string + len de la liste
+- [ ] recuperer toutes les actions faites par les utilisateur + classer --> parsing + reconnaissance de mots
+- [ ]
---
+++ test.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import json
+
+file_path = "./data/train.csv"
+
+with open(file_path, "r", encoding="utf-8") as f: 
+    lines = f.readlines()
+
+## CHARGER + DEFINIR LES COLONNES
+
+cols_set = set()
+
+df_dict = {}
+
+for l in lines:
+    sep_line = l.split(",")
+    for item in sep_line: 
+        cols_set.add(item)
+
+with open("columns.json", "w", encoding="utf-8") as f:
+    json.dump(list(cols_set), f, ensure_ascii=False, indent=2)
+
+
+## COLUMNS LOADING
+# with open("columns.json", "w", encoding="utf-8") as f:
+#     columns = list(json.load(f))
+
+# print(columns)
+
